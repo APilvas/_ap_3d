9ff4eecb1c7f05ff966ac66fb101c9209f4fea93
APilvas <pilvas@mail.ru>
2024-10-28 12:25:14 +0500

    first commit

---
+++ ap_3d/lib/app/app.dart
@@ -0,0 +1,19 @@
+
+
+import 'package:ap_3d/pages/login_page.dart';
+import 'package:ap_3d/theme/theme.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: DoDidDoneTheme.lightTheme,
+      home: const LoginPage(),
+    );
+  }
+}
---
+++ ap_3d/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:ap_3d/app/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
